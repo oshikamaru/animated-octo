a874b6c5b250e3afbdcf5565a6d15b308e4e5ab7
Alexis Nguyen <alexis.nguyen@epita.fr>
2021-09-22 19:48:06 +0200

    [TO DO] Correct error in the console bar

--- game/shoot.js
+++ game/shoot.js
@@ -83,6 +83,9 @@ function player_falling()
     for (var i = 0; i < length; i++) {
         element = noGround[i];
 
+        if (element == undefined)
+            break;
+
         var tileX = (element[0]) | 0;
         var tileY = (element[1]) | 0;
         var mtileX = (element[0] + sizeOfTileX) | 0;
